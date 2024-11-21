b024a597487b83e1f46946efb5bc6bf7bece6eb5
Alvi Aziz <ommarashid@gmail.com>
2024-11-21 10:37:39 -0500

    change 1 sec poll to 30 sec

--- React/src/Components/spotify-data/playback.jsx
+++ React/src/Components/spotify-data/playback.jsx
@@ -19,7 +19,7 @@ const Playback = ({ authToken }) => {
     useEffect(() => {
         getCurrentPlayback();
         currentSongRef.current = currentSongState;
-        const interval = setInterval(getCurrentPlayback, 1000);
+        const interval = setInterval(getCurrentPlayback, 30000);
 		return () => {
 			clearInterval(interval);
 		};
