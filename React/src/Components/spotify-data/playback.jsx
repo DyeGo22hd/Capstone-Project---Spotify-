7a442277b6bc8190c4f5c09b27c9299c6b74367a
Alvi Aziz <ommarashid@gmail.com>
2024-11-21 10:39:20 -0500

    change 30 sec to 90

--- React/src/Components/spotify-data/playback.jsx
+++ React/src/Components/spotify-data/playback.jsx
@@ -19,7 +19,7 @@ const Playback = ({ authToken }) => {
     useEffect(() => {
         getCurrentPlayback();
         currentSongRef.current = currentSongState;
-        const interval = setInterval(getCurrentPlayback, 30000);
+        const interval = setInterval(getCurrentPlayback, 90000);
 		return () => {
 			clearInterval(interval);
 		};
